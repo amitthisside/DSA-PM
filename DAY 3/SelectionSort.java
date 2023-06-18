399d420f42763d5b871c982137270e79f833903f
Amit Mishra <90273277+amitthisside@users.noreply.github.com>
2023-06-18 14:34:22 +0530

    Create SelectionSort.java

---
+++ DAY 3/SelectionSort.java
@@ -0,0 +1,12 @@
+static void sort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+            }
+            swap(arr, i, min_idx);
+        }
+    }
